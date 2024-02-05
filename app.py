c8d62e97a62932444fff9ad3f1350d225e52a7da
Shreyash Jagtap <shreyash5522@gmail.com>
2024-02-06 01:28:00 +0530

    6th-Feb-2024 | Intial Working

---
+++ app.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
+from flask_bcrypt import Bcrypt
+from forms import RegistrationForm, LoginForm, NoteForm 
+from models import User, Note  
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:%rxUXm*4H3p~80z@localhost/sflask'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    notes = db.relationship('Note', backref='author', lazy=True)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+@app.route('/home')
+
+@login_required
+def index():
+    notes = current_user.notes
+    return render_template('index.html', notes=notes)
+
+@app.route('/add_note', methods=['GET', 'POST'])
+@login_required
+def add_note():
+    form = NoteForm()
+
+    if form.validate_on_submit():
+        new_note = Note(content=form.content.data, author=current_user)
+        db.session.add(new_note)
+        db.session.commit()
+        flash('Note added successfully!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('add_note.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You can now log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            return redirect(url_for('notes'))
+        else:
+            flash('Login unsuccessful. Please check your email and password.', 'danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/notes')
+@login_required
+def notes():
+    return render_template('notes.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
